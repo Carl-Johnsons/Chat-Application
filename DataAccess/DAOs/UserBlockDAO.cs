9b41b3e34d717fa7a08fb10e8dd1414fabdff9fd
Hieu122003 <HieuNTCE171062@fpt.edu.vn>
2023-10-17 17:23:03 +0700

    Hieu 17/10 Repo and DAO

---
+++ DataAccess/DAOs/UserBlockDAO.cs
@@ -0,0 +1,109 @@
+﻿using BussinessObject.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace DataAccess.DAOs
+{
+    public class UserBlockDAO : ChatApplicationContext
+    {
+        //using singleton to access db by one instance variable
+        private static UserBlockDAO instance = null;
+        private static readonly object instanceLock = new object();
+        private UserBlockDAO() { }
+        public static UserBlockDAO Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new UserBlockDAO();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public IEnumerable<UserBlock> GetBlockedUserList()
+        {
+            using var context = new ChatApplicationContext();
+            var usersBlocked = context.UserBlocks.Include( ub => ub.BlockedUser).Include( ub => ub.User ).ToList();
+            return usersBlocked;
+        }
+
+        public UserBlock GetBlockedUserByID(int blockedUserId)
+        {
+            UserBlock userBlock = null;
+            try
+            {
+                using var context = new ChatApplicationContext();
+                userBlock = context.UserBlocks.SingleOrDefault(u => u.BlockedUserId == blockedUserId);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return userBlock;
+        }
+
+        public int AddUserBlock(UserBlock userBlock)
+        {
+
+            if (userBlock == null)
+            {
+                using var context = new ChatApplicationContext();
+
+                context.UserBlocks.Add(userBlock);
+                return context.SaveChanges();
+
+            }
+            else
+            {
+                return 0;
+            }
+            
+        }
+
+        public int UpdateUserBlock(UserBlock updateUserBlock)
+        {
+            if (updateUserBlock == null)
+            {
+                return 0;
+            }
+            using var context = new ChatApplicationContext();
+            var userBlock = GetBlockedUserByID(updateUserBlock.UserId);
+            if (userBlock == null)
+            {
+                throw new Exception("User not exist");
+            }
+            userBlock.BlockedUserId = updateUserBlock.BlockedUserId;
+            
+            context.UserBlocks.Update(userBlock);
+            
+            return context.SaveChanges();
+        }
+
+        public int RemoveUserBlocked(int blockedUserID)
+        {
+            UserBlock userblocked = GetBlockedUserByID(blockedUserID);
+            if (userblocked != null)
+            {
+                using var context = new ChatApplicationContext();
+                context.UserBlocks.Remove(userblocked);
+                return context.SaveChanges();
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ DataAccess/DAOs/UserDAO.cs
@@ -0,0 +1,113 @@
+﻿using BussinessObject.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAOs
+{
+    internal class UserDAO
+    {
+        //using singleton to access db by one instance variable
+        private static UserDAO instance = null;
+        private static readonly object instanceLock = new object();
+        private UserDAO() { }
+        public static UserDAO Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new UserDAO();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public IEnumerable<User> GetUserList()
+        {
+            using var context = new ChatApplicationContext();
+            var users = context.Users.Include(u => u.GroupGroupDeputies).Include(u => u.GroupGroupLeaders).Include(u => u.Messages).ToList();
+            return users;
+        }
+
+        public User GetUserByID(int userId)
+        {
+            User user = null;
+            try
+            {
+                using var context = new ChatApplicationContext();
+                user = context.Users.SingleOrDefault(u => u.UserId == userId);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return user;
+        }
+
+        public int AddUser(User user)
+        {
+
+            User _user = GetUserByID(user.UserId);
+            if (_user == null)
+            {
+                using var context = new ChatApplicationContext();
+                context.Users.Add(user);
+                context.SaveChanges();
+
+                return context.SaveChanges();
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public int UpdateUser(User userUpdate)
+        {
+            if (userUpdate == null)
+            {
+                return 0;
+            }
+            using var context = new ChatApplicationContext();
+            var user = GetUserByID(userUpdate.UserId);
+            if (user == null)
+            {
+                return 0;
+            }
+            user.UserId = userUpdate.UserId;
+
+            context.Users.Update(userUpdate);
+
+            return context.SaveChanges();
+        }
+
+        public int RemoveUser(int userId)
+        {
+
+            User user = GetUserByID(userId);
+            if (user != null)
+            {
+                using var context = new ChatApplicationContext();
+                context.Users.Remove(user);
+                return context.SaveChanges();
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    }
+
+}
+}
