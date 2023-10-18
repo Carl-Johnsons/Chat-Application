88b96fcd07249a448f5d5ae06ad10db364409362
tuan1703 <tuanntthpt2018@gmail.com>
2023-10-18 11:19:42 +0700

    Update DAO

---
+++ DataAccess/DAOs/GroupMessageDAO.cs
@@ -0,0 +1,123 @@
+﻿using BussinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAOs
+{
+    public class GroupMessageDAO
+    {
+        private static GroupMessageDAO instance = null;
+        private static readonly object instanceLock = new object();
+        public static GroupMessageDAO Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new GroupMessageDAO();
+                    }
+                    return instance;
+                }
+
+            }
+        }
+        public List<GroupMessage> GetGroupMessages()
+        {
+            using var context = new ChatApplicationContext();
+            var groupMessages = context.GroupMessages.ToList();
+            return groupMessages;
+        }
+        public GroupMessage GetGroupMessageByID(int MessageId)
+        {
+            GroupMessage groupMessage = null;
+            try
+            {
+                using var context = new ChatApplicationContext();
+                groupMessage = context.GroupMessages.SingleOrDefault(gm => gm.MessageId == MessageId);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return groupMessage;
+
+        }
+        public void AddMessage(GroupMessage groupMessage)
+        {
+
+            try
+            {
+                GroupMessage _groupMessage = GetGroupMessageByID(groupMessage.MessageId);
+                if (_groupMessage == null)
+                {
+                    using var context = new ChatApplicationContext();
+                    context.GroupMessages.Add(groupMessage);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("The group message is already exist.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+        public void UpdateMessage(GroupMessage groupMessage)
+        {
+
+            try
+            {
+                GroupMessage _groupMessage = GetGroupMessageByID(groupMessage.MessageId);
+                if (_groupMessage != null)
+                {
+                    using var context = new ChatApplicationContext();
+                    context.GroupMessages.Update(groupMessage);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("The group message does not already exist.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+        public void DeleteMessage(int messageId)
+        {
+
+            try
+            {
+                GroupMessage _groupMessage = GetGroupMessageByID(messageId);
+                if (_groupMessage != null)
+                {
+                    using var context = new ChatApplicationContext();
+                    context.GroupMessages.Remove(_groupMessage);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("The group message does not already exist.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ DataAccess/DAOs/ImageMessageDAO.cs
@@ -0,0 +1,100 @@
+﻿using BussinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAOs
+{
+    public class ImageMessageDAO
+    {
+        private static ImageMessageDAO instance = null;
+        private static readonly object instanceLock = new object();
+        public static ImageMessageDAO Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new ImageMessageDAO();
+                    }
+                    return instance;
+                }
+
+            }
+        }
+        public IEnumerable<ImageMessage> GetImagesMessageList()
+        {
+            using var context = new ChatApplicationContext();
+            var imageMessages = context.ImageMessages.ToList();
+            return imageMessages;
+        }
+        public ImageMessage GetImageMessageByID(int MessageId)
+        {
+            ImageMessage imageMessage = null;
+            try
+            {
+                using var context = new ChatApplicationContext();
+                imageMessage = context.ImageMessages.SingleOrDefault(img => img.MessageId == MessageId);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return imageMessage;
+
+        }
+        public void AddImageMessage(ImageMessage imageMessage)
+        {
+
+            try
+            {
+                ImageMessage _imageMessage = GetImageMessageByID(imageMessage.MessageId);
+                if (_imageMessage == null)
+                {
+                    using var context = new ChatApplicationContext();
+                    context.ImageMessages.Add(imageMessage);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("The image message is already exist.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+        
+        public void DeleteImageMessage(int messageId)
+        {
+
+            try
+            {
+                ImageMessage _imageMessage = GetImageMessageByID(messageId);
+                if (_imageMessage != null)
+                {
+                    using var context = new ChatApplicationContext();
+                    context.ImageMessages.Remove(_imageMessage);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("The image message does not already exist.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+    }
+}
