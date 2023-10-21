fbb5460fb7ed94100b2439ec99a57aa78cbc7bc6
lebathinh2003 <thefanofanimeromanceandcomedy@gmail.com>
2023-10-21 13:01:59 +0700

    vua pull tu webview ve

---
+++ ChatAPI/Controllers/MessageController.cs
@@ -0,0 +1,19 @@
+﻿using BussinessObject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        public static List<Message> Messages = new List<Message>();
+        private 
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return 
+        }
+    }
+}
