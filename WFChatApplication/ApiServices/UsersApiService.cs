dfcab713c58a48b324ad4614d6c8b99fe4d8650f
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2023-10-31 15:41:37 +0700

    add API Service

---
+++ WFChatApplication/ApiServices/UsersApiService.cs
@@ -0,0 +1,37 @@
+﻿using BussinessObject.Models;
+using System.Net.Http.Json;
+
+namespace WFChatApplication.ApiServices
+{
+    public partial class ApiServices
+    {
+        public static async Task<List<User>> GetUserListAsync(string _path)
+        {
+            using (var client = new HttpClient())
+            {
+                List<User> users = null;
+                HttpResponseMessage response = await client.GetAsync(_path);
+                if (response.IsSuccessStatusCode)
+                {
+                    users = await response.Content.ReadFromJsonAsync<List<User>>();
+                }
+                return users;
+            }
+        }
+
+
+        public static async Task<User> GetUserAsync(string _path)
+        {
+            using (var client = new HttpClient())
+            {
+                User user = null;
+                HttpResponseMessage response = await client.GetAsync(_path);
+                if (response.IsSuccessStatusCode)
+                {
+                    user = await response.Content.ReadFromJsonAsync<User>();
+                }
+                return user;
+            }
+        }
+    }
+}
