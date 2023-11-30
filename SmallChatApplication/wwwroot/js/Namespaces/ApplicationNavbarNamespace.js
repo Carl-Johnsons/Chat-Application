b54d5afa49400727269de8571fe2c14f743e5f06
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2023-12-01 06:01:20 +0700

    Remove all old file (Namespace) Done refactoring old code

--- SmallChatApplication/wwwroot/js/Namespaces/ApplicationNavbarNamespace.js
+++
@@ -1,7 +0,0 @@
-﻿
-
-ApplicationNavbarNamespace.LoadData = function loadData(user) {
-    const APPLICATION_NAVBAR = $(".application-nav-bar");
-    const avatarImg = APPLICATION_NAVBAR.find("a.nav-link#info-pop-up-container > img.avatar-img");
-    avatarImg.attr("src", user.avatarUrl);
-}
