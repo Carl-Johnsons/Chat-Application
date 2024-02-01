57f1a7e28ba4960c3a7b8c8a26ffbb177e4e8898
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-02-01 20:58:55 +0700

    add interactivity for small screen

--- reactapp/src/pages/Home/Home.tsx
+++ reactapp/src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 // Hooks
-import { useEffect, useRef } from "react";
+import { LegacyRef, useCallback, useEffect, useRef } from "react";
 import { useGlobalState } from "../../GlobalState";
 //Components
 import NavigationBar from "../../Components/NavigationBar";
@@ -24,8 +24,26 @@ const Home = () => {
   //Local state
   const conn = useSignalRConnection(import.meta.env.VITE_SIGNALR_URL);
   //Ref
-  const sideContentRef = useRef(null);
-  const rightRef = useRef(null);
+  const navRef: LegacyRef<HTMLDivElement> = useRef(null);
+  const leftRef: LegacyRef<HTMLDivElement> = useRef(null);
+  const rightRef: LegacyRef<HTMLDivElement> = useRef(null);
+  const handleClick = useCallback((leftShow: boolean) => {
+    if (!leftRef.current || !rightRef.current || !navRef.current) {
+      return;
+    }
+    if (window.matchMedia("(min-width: 768px)").matches) {
+      return;
+    }
+    if (leftShow) {
+      leftRef.current.classList.remove("d-none");
+      rightRef.current.classList.add("d-none");
+      rightRef.current.classList.remove("d-flex");
+    } else {
+      leftRef.current.classList.add("d-none");
+      rightRef.current.classList.remove("d-none");
+      rightRef.current.classList.add("d-flex");
+    }
+  }, []);
 
   useEffect(() => {
     conn && setConnection(conn);
@@ -46,28 +64,30 @@ const Home = () => {
     fetchUserData();
   }, [setFriendRequestList, setUserId, userId]);
 
-  useEffect(() => {});
-
   return (
     <div className={cx("container-fluid", "p-0", "d-flex", "w-100", "h-100")}>
-      <div className={cx("navbar-section", "flex-shrink-0")}>
+      <div
+        ref={navRef}
+        onClick={() => handleClick(true)}
+        className={cx("navbar-section", "flex-shrink-0")}
+      >
         <NavigationBar />
       </div>
       <div
+        ref={leftRef}
+        onClick={() => handleClick(false)}
         className={cx(
           "left",
-          "d-flex",
           "flex-grow-1",
           "flex-md-grow-0",
           "flex-shrink-1",
           "flex-sm-shrink-0"
         )}
       >
-        <div className={cx("sidebar-section", "flex-grow-1", "flex-shrink-1")}>
-          <SidebarContent />
-        </div>
+        <SidebarContent />
       </div>
       <div
+        ref={rightRef}
         className={cx(
           "right",
           "d-none",
