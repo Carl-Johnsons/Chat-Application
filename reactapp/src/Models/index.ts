52cc9350773bc603f9f4c30466b884067bf30868
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-02-08 06:42:02 +0700

    delete unnecessary file

--- reactapp/src/Models/index.ts
+++
@@ -1,9 +0,0 @@
-export type { Friend } from "./Friend";
-export type { FriendRequest } from "./FriendRequest";
-export type { IndividualMessage } from "./IndividualMessage";
-export type { JwtToken } from "./JwtToken";
-export type { Message } from "./Message";
-export type {ModalType} from "./ModalType";
-export type { RefreshToken } from "./RefreshToken";
-export type { RequestMethod } from "./RequestMethod";
-export type { User } from "./User";
