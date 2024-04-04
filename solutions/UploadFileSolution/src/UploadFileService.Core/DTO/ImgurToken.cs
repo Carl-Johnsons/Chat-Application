1c077de2f228846cee96ceffcf5cc744de0b50a9
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-04-04 14:11:27 +0700

    create UploadSolution

---
+++ solutions/UploadFileSolution/src/UploadFileService.Core/DTO/ImgurToken.cs
@@ -0,0 +1,21 @@
+﻿
+using Newtonsoft.Json;
+
+namespace UploadFileService.Core.DTO;
+public class ImgurToken
+{
+    [JsonProperty("access_token")]
+    public string AccessToken { get; set; } = null!;
+    [JsonProperty("expires_in")]
+    public int ExpiresIn { get; set; }
+    [JsonProperty("token_type")]
+    public string TokenType { get; set; } = null!;
+    [JsonProperty("scope")]
+    public string Scope { get; set; } = null!;
+    [JsonProperty("refresh_token")]
+    public string RefreshToken { get; set; } = null!;
+    [JsonProperty("account_id")]
+    public int AccountId { get; set; }
+    [JsonProperty("account_username")]
+    public string AccountUsername { get; set; } = null!;
+}
