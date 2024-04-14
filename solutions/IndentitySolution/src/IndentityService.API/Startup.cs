5b94752d788637591ff687d036d5144c0e238bb4
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-04-14 16:28:13 +0700

    Add database for indentity server4

--- solutions/IndentitySolution/src/IndentityService.API/Startup.cs
+++ solutions/IndentitySolution/src/IndentityService.API/Startup.cs
@@ -3,8 +3,12 @@
 
 
 using IdentityServer4;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServerHost.Quickstart.UI;
 using IndentityService.API.Data;
 using IndentityService.API.Models;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -13,78 +17,140 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace IndentityService.API
+using System.Linq;
+
+namespace IndentityService.API;
+
+public class Startup
 {
-    public class Startup
+    public IWebHostEnvironment Environment { get; }
+    public IConfiguration Configuration { get; }
+
+    public Startup(IWebHostEnvironment environment, IConfiguration configuration)
     {
-        public IWebHostEnvironment Environment { get; }
-        public IConfiguration Configuration { get; }
+        Environment = environment;
+        Configuration = configuration;
+    }
 
-        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
+    public void ConfigureServices(IServiceCollection services)
+    {
+        DotNetEnv.Env.Load(".env.development");
+
+        var server = DotNetEnv.Env.GetString("SERVER", "Not found");
+        var db = DotNetEnv.Env.GetString("DB", "Not found");
+        var pwd = DotNetEnv.Env.GetString("SA_PASSWORD", "Not found");
+        var connectionString = $"Server={server};Database={db};User Id=sa;Password='{pwd}';TrustServerCertificate=true";
+
+
+        services.AddControllersWithViews();
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        var migrationAssembly = typeof(Startup).Assembly.GetName().Name;
+
+
+        // Persist indentity server data to database through ef-core
+        var builder = services.AddIdentityServer(options =>
         {
-            Environment = environment;
-            Configuration = configuration;
+            options.Events.RaiseErrorEvents = true;
+            options.Events.RaiseInformationEvents = true;
+            options.Events.RaiseFailureEvents = true;
+            options.Events.RaiseSuccessEvents = true;
+
+            // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+            options.EmitStaticAudienceClaim = true;
+        }).AddTestUsers(TestUsers.Users)
+          .AddConfigurationStore(options =>
+          {
+              options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+                                            sql => sql.MigrationsAssembly(migrationAssembly));
+          })
+          .AddOperationalStore(options =>
+          {
+              options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+                                            sql => sql.MigrationsAssembly(migrationAssembly));
+          });
+
+        // not recommended for production - you need to store your key material somewhere secure
+        builder.AddDeveloperSigningCredential();
+
+        services.AddAuthentication()
+            .AddGoogle(options =>
+            {
+                options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+                // register your IdentityServer with Google at https://console.developers.google.com
+                // enable the Google+ API
+                // set the redirect URI to https://localhost:5001/signin-google
+                options.ClientId = "copy client ID from Google here";
+                options.ClientSecret = "copy client secret from Google here";
+            });
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        // This method should be called only once if indentity database didn't exist
+        InitializeDatabase(app);
+
+        if (Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseDatabaseErrorPage();
         }
 
-        public void ConfigureServices(IServiceCollection services)
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseIdentityServer();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
         {
-            services.AddControllersWithViews();
+            endpoints.MapDefaultControllerRoute();
+        });
+    }
 
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+    private void InitializeDatabase(IApplicationBuilder app)
+    {
+        using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+        {
+            var serviceProvider = serviceScope.ServiceProvider;
+            serviceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+            var context = serviceProvider.GetRequiredService<ConfigurationDbContext>();
+            context.Database.Migrate();
 
-            var builder = services.AddIdentityServer(options =>
+            // Seed data
+            if (!context.Clients.Any())
             {
-                options.Events.RaiseErrorEvents = true;
-                options.Events.RaiseInformationEvents = true;
-                options.Events.RaiseFailureEvents = true;
-                options.Events.RaiseSuccessEvents = true;
-
-                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
-                options.EmitStaticAudienceClaim = true;
-            })
-                .AddInMemoryIdentityResources(Config.IdentityResources)
-                .AddInMemoryApiScopes(Config.ApiScopes)
-                .AddInMemoryClients(Config.Clients)
-                .AddAspNetIdentity<ApplicationUser>();
-
-            // not recommended for production - you need to store your key material somewhere secure
-            builder.AddDeveloperSigningCredential();
-
-            services.AddAuthentication()
-                .AddGoogle(options =>
+                foreach (var client in Config.Clients)
                 {
-                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-
-                    // register your IdentityServer with Google at https://console.developers.google.com
-                    // enable the Google+ API
-                    // set the redirect URI to https://localhost:5001/signin-google
-                    options.ClientId = "copy client ID from Google here";
-                    options.ClientSecret = "copy client secret from Google here";
-                });
-        }
-
-        public void Configure(IApplicationBuilder app)
-        {
-            if (Environment.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
+                    context.Clients.Add(client.ToEntity());
+                }
+                context.SaveChanges();
             }
 
-            app.UseStaticFiles();
-
-            app.UseRouting();
-            app.UseIdentityServer();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
+            if (!context.IdentityResources.Any())
             {
-                endpoints.MapDefaultControllerRoute();
-            });
+                foreach (var identityResource in Config.IdentityResources)
+                {
+                    context.IdentityResources.Add(identityResource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+            if (!context.ApiScopes.Any())
+            {
+                foreach (var resource in Config.ApiScopes)
+                {
+                    context.ApiScopes.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
         }
     }
+
 }
