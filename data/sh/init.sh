4897b780fc3ff96e50222b790f5780fb26928848
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-04-30 16:20:02 +0700

    update config

--- data/sh/init.sh
+++ data/sh/init.sh
@@ -4,15 +4,15 @@ username="sa"
 password="$SA_PASSWORD" 
 script_file="create_database.sql"
 
-indentityDB="$IndentityDB"
+identityDB="$IdentityDB"
 messageDB="$MessageDB"
 
 # Create SQL script file
 cat <<EOF >$script_file
 USE master;
-IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE name = '$indentityDB')
+IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE name = '$identityDB')
 BEGIN
-    CREATE DATABASE $indentityDB;
+    CREATE DATABASE $identityDB;
 END
 IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE name = '$messageDB')
 BEGIN
@@ -23,7 +23,7 @@ EOF
 /opt/mssql-tools/bin/sqlcmd -S $server -U $username -P $password -i $script_file
 # Check if the command was successful
 if [ $? -eq 0 ]; then
-    echo "Database creation '$indentityDB', '$messageDB' script executed successfully!"
+    echo "Database creation '$identityDB', '$messageDB' script executed successfully!"
 else
     echo "Error executing database creation script"
 fi
