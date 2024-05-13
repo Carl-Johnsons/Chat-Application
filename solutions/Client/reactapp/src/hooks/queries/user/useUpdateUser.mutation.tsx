c0d39b101c8ac5c6bfe5a79afb24e0668e2068f1
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-05-13 21:04:09 +0700

    update useUpdateUser hook

--- solutions/Client/reactapp/src/hooks/queries/user/useUpdateUser.mutation.tsx
+++ solutions/Client/reactapp/src/hooks/queries/user/useUpdateUser.mutation.tsx
@@ -1,21 +1,30 @@
 import { axiosInstance } from "@/utils";
 import { User } from "@/models";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useLocalStorage } from "hooks/useStorage";
 
 /**
  * @param {User} user
  * @returns
  */
-export const updateUser = async (user: User): Promise<User | null> => {
+export const updateUser = async (
+  user: User,
+  accessToken: string
+): Promise<User | null> => {
   const url = "http://localhost:5001/api/users";
-  const response = await axiosInstance.put(url, user);
+  const response = await axiosInstance.put(url, user, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
   return response.data;
 };
 
 const useUpdateUser = () => {
+  const [getAccessToken] = useLocalStorage("access_token");
   const queryClient = useQueryClient();
   return useMutation<User | null, Error, { user: User }, unknown>({
-    mutationFn: ({ user }) => updateUser(user),
+    mutationFn: ({ user }) => updateUser(user, getAccessToken() as string),
     onSuccess: (data) => {
       queryClient.invalidateQueries({
         queryKey: ["currentUser"],
