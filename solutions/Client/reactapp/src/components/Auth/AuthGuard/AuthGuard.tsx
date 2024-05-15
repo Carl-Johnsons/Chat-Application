64c9adcff760c7c727330704c667c5da3bb7a8d5
Miu1314 <tckimngan@gmail.com>
2024-05-15 17:10:33 +0700

    fix_loading_animation

--- solutions/Client/reactapp/src/components/Auth/AuthGuard/AuthGuard.tsx
+++ solutions/Client/reactapp/src/components/Auth/AuthGuard/AuthGuard.tsx
@@ -1,12 +1,14 @@
 import userManager from "app/oidc-client";
 import { Suspense, useEffect } from "react";
 import classNames from "classnames/bind";
-import style from "./AuthGuard.module.scss"
-const cx=classNames.bind(style)
+import style from "./AuthGuard.module.scss";
+const cx = classNames.bind(style);
 const Loading = () => {
-  return <div className={cx("container")}>
-    <div className={cx("loader")}></div>;
-  </div>;
+  return (
+    <div className={cx("container")}>
+      <div className={cx("loader")}></div>;
+    </div>
+  );
 };
 
 const AuthGuard: React.FC<{ children: React.ReactNode }> = ({ children }) => {
@@ -17,11 +19,10 @@ const AuthGuard: React.FC<{ children: React.ReactNode }> = ({ children }) => {
         await userManager.signinRedirect();
       }
     };
-   handleLogin();
+    handleLogin();
   }, []);
 
   return <Suspense fallback={<Loading />}>{children}</Suspense>;
-  
 };
 
 export { AuthGuard };
