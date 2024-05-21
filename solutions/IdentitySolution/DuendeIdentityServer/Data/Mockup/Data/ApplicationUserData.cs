9f4b77db9feb54397c59fa59b7774ef945c31234
minhnhut23 <minhnhut23.work@gmail.com>
2024-05-22 01:18:02 +0700

    add-data

--- solutions/IdentitySolution/DuendeIdentityServer/Data/Mockup/Data/ApplicationUserData.cs
+++ solutions/IdentitySolution/DuendeIdentityServer/Data/Mockup/Data/ApplicationUserData.cs
@@ -48,6 +48,104 @@ public static class ApplicationUserData
                     BackgroundUrl = "https://i.pinimg.com/736x/e2/07/8b/e2078be54495947309e0f68b59346f68.jpg",
                     Active = true,
                     Dob = new DateTime(2005, 12, 3)
+                },
+                new ApplicationUser {
+                    UserName = "an",
+                    Email = "minhan@email.com",
+                    EmailConfirmed = true,
+                    PhoneNumber = "0123456786",
+                    PhoneNumberConfirmed = true,
+                    /* Custom attribute */
+                    Name = "Minh An",
+                    Gender = "Nam",
+                    AvatarUrl = "https://i.pinimg.com/736x/e2/07/8b/e2078be54495947309e0f68b59346f68.jpg",
+                    BackgroundUrl = "https://i.pinimg.com/736x/e2/07/8b/e2078be54495947309e0f68b59346f68.jpg",
+                    Active = true,
+                    Dob = new DateTime(2005, 12, 3)
+                },
+                new ApplicationUser {
+                    UserName = "kian",
+                    Email = "kianstrong@email.com",
+                    EmailConfirmed = true,
+                    PhoneNumber = "0123456785",
+                    PhoneNumberConfirmed = true,
+                    /* Custom attribute */
+                    Name = "Kian Strong",
+                    Gender = "Nam",
+                    AvatarUrl = "https://i.pinimg.com/736x/e2/07/8b/e2078be54495947309e0f68b59346f68.jpg",
+                    BackgroundUrl = "https://i.pinimg.com/736x/e2/07/8b/e2078be54495947309e0f68b59346f68.jpg",
+                    Active = true,
+                    Dob = new DateTime(2005, 12, 3)
+                },
+                new ApplicationUser {
+                    UserName = "cara",
+                    Email = "cararose@email.com",
+                    EmailConfirmed = true,
+                    PhoneNumber = "0123456784",
+                    PhoneNumberConfirmed = true,
+                    /* Custom attribute */
+                    Name = "Cara Rose",
+                    Gender = "Nam",
+                    AvatarUrl = "https://i.pinimg.com/736x/e2/07/8b/e2078be54495947309e0f68b59346f68.jpg",
+                    BackgroundUrl = "https://i.pinimg.com/736x/e2/07/8b/e2078be54495947309e0f68b59346f68.jpg",
+                    Active = true,
+                    Dob = new DateTime(2005, 12, 3)
+                },
+                new ApplicationUser {
+                    UserName = "raina",
+                    Email = "rainaduarte@email.com",
+                    EmailConfirmed = true,
+                    PhoneNumber = "0123456783",
+                    PhoneNumberConfirmed = true,
+                    /* Custom attribute */
+                    Name = "Raina Duarte",
+                    Gender = "Nam",
+                    AvatarUrl = "https://i.pinimg.com/736x/e2/07/8b/e2078be54495947309e0f68b59346f68.jpg",
+                    BackgroundUrl = "https://i.pinimg.com/736x/e2/07/8b/e2078be54495947309e0f68b59346f68.jpg",
+                    Active = true,
+                    Dob = new DateTime(2005, 12, 3)
+                },
+                new ApplicationUser {
+                    UserName = "mac",
+                    Email = "macnelson@email.com",
+                    EmailConfirmed = true,
+                    PhoneNumber = "0123456782",
+                    PhoneNumberConfirmed = true,
+                    /* Custom attribute */
+                    Name = "Mac Nelson",
+                    Gender = "Nam",
+                    AvatarUrl = "https://i.pinimg.com/736x/e2/07/8b/e2078be54495947309e0f68b59346f68.jpg",
+                    BackgroundUrl = "https://i.pinimg.com/736x/e2/07/8b/e2078be54495947309e0f68b59346f68.jpg",
+                    Active = true,
+                    Dob = new DateTime(2005, 12, 3)
+                },
+                new ApplicationUser {
+                    UserName = "lainey",
+                    Email = "laineyhart@email.com",
+                    EmailConfirmed = true,
+                    PhoneNumber = "0123456781",
+                    PhoneNumberConfirmed = true,
+                    /* Custom attribute */
+                    Name = "Lainey Hart",
+                    Gender = "Nam",
+                    AvatarUrl = "https://i.pinimg.com/736x/e2/07/8b/e2078be54495947309e0f68b59346f68.jpg",
+                    BackgroundUrl = "https://i.pinimg.com/736x/e2/07/8b/e2078be54495947309e0f68b59346f68.jpg",
+                    Active = true,
+                    Dob = new DateTime(2005, 12, 3)
+                },
+                new ApplicationUser {
+                    UserName = "willa",
+                    Email = "willapark@email.com",
+                    EmailConfirmed = true,
+                    PhoneNumber = "0123456780",
+                    PhoneNumberConfirmed = true,
+                    /* Custom attribute */
+                    Name = "Willa Park",
+                    Gender = "Nam",
+                    AvatarUrl = "https://i.pinimg.com/736x/e2/07/8b/e2078be54495947309e0f68b59346f68.jpg",
+                    BackgroundUrl = "https://i.pinimg.com/736x/e2/07/8b/e2078be54495947309e0f68b59346f68.jpg",
+                    Active = true,
+                    Dob = new DateTime(2005, 12, 3)
                 }
     ];
 }
