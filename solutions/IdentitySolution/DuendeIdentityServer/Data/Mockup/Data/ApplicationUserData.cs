3f089e7a1b8d7b10c00670ad7bda599b2872dbbc
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-05-23 03:23:05 +0700

    hardcode mockup user Id

--- solutions/IdentitySolution/DuendeIdentityServer/Data/Mockup/Data/ApplicationUserData.cs
+++ solutions/IdentitySolution/DuendeIdentityServer/Data/Mockup/Data/ApplicationUserData.cs
@@ -7,6 +7,7 @@ public static class ApplicationUserData
     public static IEnumerable<ApplicationUser> Data =>
              [
                 new ApplicationUser{
+                    Id="61c61ac7-291e-4075-9689-666ef05547ed",
                     UserName = "alice",
                     Email = "AliceSmith@email.com",
                     EmailConfirmed = true,
@@ -21,6 +22,7 @@ public static class ApplicationUserData
                     Dob = new DateTime(2005, 12, 3)
                 },
                 new ApplicationUser{
+                    Id="078ecc42-7643-4cff-b851-eeac5ba1bb29",
                     UserName = "bob",
                     Email = "BobSmith@email.com",
                     EmailConfirmed = true,
@@ -36,6 +38,7 @@ public static class ApplicationUserData
                 },
                 new ApplicationUser
                 {
+                    Id="1cfb7c40-cccc-4a87-88a9-ff967d8dcddb",
                     UserName = "duc",
                     Email = "taiduc@email.com",
                     EmailConfirmed = true,
@@ -50,6 +53,7 @@ public static class ApplicationUserData
                     Dob = new DateTime(2005, 12, 3)
                 },
                 new ApplicationUser {
+                    Id="50e00c7f-39da-48d1-b273-3562225a5972",
                     UserName = "an",
                     Email = "minhan@email.com",
                     EmailConfirmed = true,
@@ -64,6 +68,7 @@ public static class ApplicationUserData
                     Dob = new DateTime(2005, 12, 3)
                 },
                 new ApplicationUser {
+                    Id="bb06e4ec-f371-45d5-804e-22c65c77f67d",
                     UserName = "kian",
                     Email = "kianstrong@email.com",
                     EmailConfirmed = true,
@@ -78,6 +83,7 @@ public static class ApplicationUserData
                     Dob = new DateTime(2005, 12, 3)
                 },
                 new ApplicationUser {
+                    Id="594a3fc8-3d24-4305-a9d7-569586d0604e",
                     UserName = "cara",
                     Email = "cararose@email.com",
                     EmailConfirmed = true,
@@ -92,6 +98,7 @@ public static class ApplicationUserData
                     Dob = new DateTime(2005, 12, 3)
                 },
                 new ApplicationUser {
+                    Id="03e4b46e-b84a-43a9-a421-1b19e02023bb",
                     UserName = "raina",
                     Email = "rainaduarte@email.com",
                     EmailConfirmed = true,
@@ -106,6 +113,7 @@ public static class ApplicationUserData
                     Dob = new DateTime(2005, 12, 3)
                 },
                 new ApplicationUser {
+                    Id="cd1c7fe9-3308-4afb-83f4-23fa1e9efba8",
                     UserName = "mac",
                     Email = "macnelson@email.com",
                     EmailConfirmed = true,
@@ -120,6 +128,7 @@ public static class ApplicationUserData
                     Dob = new DateTime(2005, 12, 3)
                 },
                 new ApplicationUser {
+                    Id="76346f0e-a52c-4d94-a909-4a8cc59c8ede",
                     UserName = "lainey",
                     Email = "laineyhart@email.com",
                     EmailConfirmed = true,
@@ -134,6 +143,7 @@ public static class ApplicationUserData
                     Dob = new DateTime(2005, 12, 3)
                 },
                 new ApplicationUser {
+                    Id="e797952f-1b76-4db9-81a4-8e2f5f9152ea",
                     UserName = "willa",
                     Email = "willapark@email.com",
                     EmailConfirmed = true,
