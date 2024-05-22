e37b015f7a5c429b9740f410c4f857fa4579345a
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-05-22 18:42:52 +0700

    edit file dependency injection in upload service

--- solutions/UploadFileSolution/src/UploadFileService.Application/DependencyInjection.cs
+++ solutions/UploadFileSolution/src/UploadFileService.Application/DependencyInjection.cs
@@ -1,6 +1,32 @@
-﻿namespace UploadFileService.Application;
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
-public class DependencyInjection
+namespace UploadFileService.Application;
+
+public static class DependencyInjection
 {
+    public static IServiceCollection AddApplicationService(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddMassTransit(busConfig =>
+        {
+            busConfig.SetKebabCaseEndpointNameFormatter();
 
+
+            busConfig.UsingRabbitMq((context, config) =>
+            {
+                config.Host(new Uri("amqp://rabbitmq/"), h =>
+                {
+                    h.Username("admin");
+                    h.Password("pass");
+                });
+
+                config.ConfigureEndpoints(context);
+            });
+        });
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        return services;
+    }
 }
