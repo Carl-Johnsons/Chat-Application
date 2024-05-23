8606d92a24059e76ac75be8a32eb490da6ce6396
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-05-23 21:31:12 +0700

    add more event in Contract prk

---
+++ solutions/Contract/Contract/Common/BaseAuditableEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Contract.Common;
+
+public class BaseAuditableEntity : BaseEntity
+{
+    [Column]
+    public DateTime CreatedAt { get; set; }
+    [Column]
+    public DateTime UpdatedAt { get; set; }
+}
