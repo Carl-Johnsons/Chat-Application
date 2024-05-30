57f895208aadbe67e8df37c9baa73f6911458800
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-05-30 23:07:35 +0700

    add tinyMCE editor

---
+++ solutions/Client/reactapp/src/components/PostView/PostInputContainer/PostInput.container.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { useGetCurrentUser } from "@/hooks/queries/user";
+
+const PostInputContainer = () => {
+  const { data: userData } = useGetCurrentUser();
+  const plugins = [
+    "anchor",
+    "autolink",
+    "charmap",
+    "codesample",
+    "emoticons",
+    "image",
+    "link",
+    "lists",
+    "media",
+    "searchreplace",
+    "table",
+    "visualblocks",
+    "wordcount",
+    "checklist",
+    "mediaembed",
+    "casechange",
+    "export",
+    "formatpainter",
+    "pageembed",
+    "linkchecker",
+    "a11ychecker",
+    "tinymcespellchecker",
+    "permanentpen",
+    "powerpaste",
+    "advtable",
+    "advcode",
+    "editimage",
+    "advtemplate",
+    "ai",
+    "mentions",
+    "tinycomments",
+    "tableofcontents",
+    "footnotes",
+    "mergetags",
+    "autocorrect",
+    "typography",
+    "inlinecss",
+    "markdown",
+  ];
+
+  return (
+    <Editor
+      apiKey={process.env.NEXT_PUBLIC_TINYMCE_API_KEY}
+      init={{
+        plugins: plugins,
+        // "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown",
+        toolbar:
+          "undo redo | " +
+          "blocks fontfamily fontsize | " +
+          "bold italic underline strikethrough | " +
+          "link image media table mergetags | " +
+          "addcomment showcomments | " +
+          "spellcheckdialog a11ycheck typography | " +
+          "align lineheight | " +
+          "checklist numlist bullist indent outdent | " +
+          "emoticons charmap | " +
+          "removeformat",
+        tinycomments_mode: "embedded",
+        tinycomments_author: userData?.name ?? "",
+        mergetags_list: [
+          { value: "First.Name", title: "First Name" },
+          { value: "Email", title: "Email" },
+        ],
+        ai_request: (request, respondWith) =>
+          respondWith.string(() =>
+            Promise.reject("See docs to implement AI Assistant")
+          ),
+      }}
+      initialValue="Write your thought!"
+    />
+  );
+};
+
+export { PostInputContainer };
