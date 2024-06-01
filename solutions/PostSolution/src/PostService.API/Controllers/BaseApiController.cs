0c371d48b3106b5172a4d588d5b6299cbef2cde9
minhnhut23 <minhnhut23.work@gmail.com>
2024-06-01 18:58:33 +0700

    tag-crud

---
+++ solutions/PostSolution/src/PostService.API/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PostService.API.Controllers;
+
+public class BaseApiController : ControllerBase
+{
+    protected readonly ISender _sender;
+    protected readonly IHttpContextAccessor _httpContextAccessor;
+
+    public BaseApiController(ISender sender, IHttpContextAccessor httpContextAccessor)
+    {
+        _sender = sender;
+        _httpContextAccessor = httpContextAccessor;
+    }
+}
---
+++ solutions/PostSolution/src/PostService.Application/Tags/Commands/UpdateTagCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+
+namespace PostService.Application.Tags.Commands;
+
+public class UpdateTagCommand : IRequest<Tag>
+{
+
+    public Guid TagId { get; init; }
+    public string Value { get; init; }
+
+}
+
+public class UpdateTagCommandHandler : IRequestHandler<UpdateTagCommand, Tag>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateTagCommandHandler(IApplicationDbContext context, IUnitOfWork unitOfWork)
+    {
+        _context = context;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Tag> Handle(UpdateTagCommand request, CancellationToken cancellationToken)
+    {
+        var tag = _context.Tags
+                    .Where(c => c.Id == request.TagId)
+                    .SingleOrDefault();
+        if (tag != null)
+        {
+            _context.Tags.Add(tag);
+        }
+        await _unitOfWork.SaveChangeAsync(cancellationToken);
+
+        return tag;
+    }
+
+}
