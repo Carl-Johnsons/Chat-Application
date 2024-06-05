3d92c1c0e67cc9496d0d03cf1db2a47f331034fb
minhnhut23 <minhnhut23.work@gmail.com>
2024-06-05 16:35:44 +0700

    add post crud

---
+++ solutions/PostSolution/src/PostService.Application/Posts/Commands/CreatePostCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+namespace PostService.Application.Posts.Commands;
+
+public class CreatePostCommand : IRequest
+{
+    public string PostContent { get; init; } = null!;
+    public Guid UserId { get; init; }
+}
+
+public class CreatePostCommandHandler : IRequestHandler<CreatePostCommand>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreatePostCommandHandler(IApplicationDbContext context, IUnitOfWork unitOfWork)
+    {
+        _context = context;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Handle(CreatePostCommand request, CancellationToken cancellationToken)
+    {
+        Post post = new Post
+        {
+            Content = request.PostContent,
+            UserId = request.UserId
+        };
+
+        _context.Posts.Add(post);
+        await _unitOfWork.SaveChangeAsync();
+
+    }
+}
---
+++ solutions/PostSolution/src/PostService.Application/Posts/Queries/GetAllPostsQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace PostService.Application.Posts.Queries;
+
+public class GetAllPostsQuery : IRequest<List<Post>>;
+
+public class GetAllPostsQueryHandler : IRequestHandler<GetAllPostsQuery, List<Post>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetAllPostsQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Post>> Handle(GetAllPostsQuery request, CancellationToken cancellationToken)
+    {
+        var posts = await _context.Posts.ToListAsync();
+
+        return posts;
+    }
+}
