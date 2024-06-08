5a6ed0537543b8782e0a42ba71570aa2ee4e477c
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-06-08 19:56:49 +0700

    setup error handling in other services

---
+++ solutions/PostSolution/src/PostService.Domain/Common/ResultException.cs
@@ -0,0 +1,11 @@
+﻿namespace PostService.Domain.Common;
+public class ResultException : Exception
+{
+    public IEnumerable<Error> Errors { get; }
+
+    public ResultException(IEnumerable<Error> errors)
+        : base($"Errors:\n{string.Join("\n", errors.Select(e => $"Code: {e.Code}, Message: {e.Message}"))}")
+    {
+        Errors = errors;
+    }
+}
