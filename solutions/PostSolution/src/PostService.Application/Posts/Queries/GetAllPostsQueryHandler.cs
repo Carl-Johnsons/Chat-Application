a11077def1fc69ce31a84b44fe9042f94f19423e
minhnhut23 <minhnhut23.work@gmail.com>
2024-06-08 23:21:25 +0700

    update post crud

--- solutions/PostSolution/src/PostService.Application/Posts/Queries/GetAllPostsQueryHandler.cs
+++ solutions/PostSolution/src/PostService.Application/Posts/Queries/GetAllPostsQueryHandler.cs
@@ -1,11 +1,12 @@
 ﻿using MediatR;
 using Microsoft.EntityFrameworkCore;
+using PostService.Domain.DTOs;
 
 namespace PostService.Application.Posts.Queries;
 
-public class GetAllPostsQuery : IRequest<List<Post>>;
+public class GetAllPostsQuery : IRequest<List<PostDTO>>;
 
-public class GetAllPostsQueryHandler : IRequestHandler<GetAllPostsQuery, List<Post>>
+public class GetAllPostsQueryHandler : IRequestHandler<GetAllPostsQuery, List<PostDTO>>
 {
     private readonly IApplicationDbContext _context;
 
@@ -14,11 +15,69 @@ public class GetAllPostsQueryHandler : IRequestHandler<GetAllPostsQuery, List<Po
         _context = context;
     }
 
-    public async Task<List<Post>> Handle(GetAllPostsQuery request, CancellationToken cancellationToken)
+    public async Task<List<PostDTO>> Handle(GetAllPostsQuery request, CancellationToken cancellationToken)
     {
         var posts = await _context.Posts
-            .ToListAsync();
+                        .ToListAsync();
 
-        return posts;
+        List<PostDTO> result = new List<PostDTO>();
+
+        foreach (var p in posts)
+        {
+            var post = await _context.Posts
+                        .Where(pt => pt.Id == p.Id)
+                        .SingleOrDefaultAsync();
+
+            var tag = await _context.PostTags
+                        .Where(t => t.PostId == p.Id)
+                        .Include(t => t.Tag)
+                        .ToListAsync();
+
+            var countInteract = await _context.PostInteracts
+                        .Where(pi => pi.PostId == p.Id)
+                        .CountAsync();
+
+            List<string> tags = new List<string>();
+
+            foreach (PostTag t in tag)
+            {
+                tags.Add(t.Tag.Value);
+            }
+
+            var topInteractions = await _context.PostInteracts
+                    .Where(pi => pi.PostId == p.Id)
+                    .GroupBy(pi => pi.InteractionId)
+                    .Select(g => new PostInteractionCountDTO
+                    {
+                        InteractionId = g.Key,
+                        Count = g.Count(),
+                        Value = _context.Interactions.FirstOrDefault(iv => iv.Id == g.Key)!.Value
+                    })
+                    .OrderByDescending(pi => pi.Count)
+                    .Take(3)
+                    .ToListAsync();
+
+            List<string> interacts = new List<string>();
+
+            foreach (var t in topInteractions)
+            {
+                interacts.Add(t.Value);
+            }
+
+            var postReponse = new PostDTO
+            {
+                PostId = p.Id,
+                Content = post.Content,
+                UserId = post.UserId,
+                CreatedAt = post.CreatedAt,
+                InteractTotal = countInteract,
+                Interactions = interacts,
+                Tags = tags
+            };
+
+            result.Add(postReponse);
+        }
+
+        return result;
     }
 }
--- solutions/PostSolution/src/PostService.Application/Posts/Queries/GetPostByIdQueryHandler.cs
+++ solutions/PostSolution/src/PostService.Application/Posts/Queries/GetPostByIdQueryHandler.cs
@@ -1,13 +1,15 @@
 ﻿using MediatR;
 using Microsoft.EntityFrameworkCore;
+using PostService.Domain.DTOs;
+using PostService.Domain.Entities;
 namespace PostService.Application.Posts.Queries;
 
-public class GetPostByIdQuery : IRequest <Post>
+public class GetPostByIdQuery : IRequest<PostDTO>
 {
     public Guid Id { get; set; }
 }
 
-public class GetPostByIdQueryHandler : IRequestHandler<GetPostByIdQuery, Post>
+public class GetPostByIdQueryHandler : IRequestHandler<GetPostByIdQuery, PostDTO>
 {
     private readonly IApplicationDbContext _context;
 
@@ -16,12 +18,59 @@ public class GetPostByIdQueryHandler : IRequestHandler<GetPostByIdQuery, Post>
         _context = context;
     }
 
-    public async Task<Post> Handle(GetPostByIdQuery request, CancellationToken cancellationToken)
+    public async Task<PostDTO> Handle(GetPostByIdQuery request, CancellationToken cancellationToken)
     {
         var post = await _context.Posts
                     .Where(p => p.Id == request.Id)
                     .SingleOrDefaultAsync();
 
-        return post;
+        var tag = await _context.PostTags
+                    .Where(t => t.PostId == request.Id)
+                    .Include(t => t.Tag)
+                    .ToListAsync();
+
+        var countInteract = await _context.PostInteracts
+                    .Where(pi => pi.PostId == request.Id)
+                    .CountAsync();
+
+        List<string> tags = new List<string>();
+
+        foreach (PostTag t in tag)
+        {
+            tags.Add(t.Tag.Value);
+        }
+
+        var topInteractions = await _context.PostInteracts
+                    .Where(pi => pi.PostId == request.Id)
+                    .GroupBy(pi => pi.InteractionId)
+                    .Select(g => new PostInteractionCountDTO
+                    {
+                        InteractionId = g.Key,
+                        Count = g.Count(),
+                        Value = _context.Interactions.FirstOrDefault(iv => iv.Id == g.Key)!.Value
+                    })
+                    .OrderByDescending(pi => pi.Count)
+                    .Take(3)
+                    .ToListAsync();
+
+        List<string> interacts = new List<string>();
+
+        foreach (var t in topInteractions)
+        {
+            interacts.Add(t.Value);
+        }
+
+        var postReponse = new PostDTO
+        {
+            PostId = request.Id,
+            Content = post.Content,
+            UserId = post.UserId,
+            CreatedAt = post.CreatedAt,
+            InteractTotal = countInteract,
+            Interactions = interacts,
+            Tags = tags
+        };
+
+        return postReponse;
     }
 }
