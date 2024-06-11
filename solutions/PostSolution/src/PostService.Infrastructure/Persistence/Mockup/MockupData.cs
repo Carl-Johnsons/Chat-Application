a4b8c5611361b3def63f1504f09a1bf60ea1ea8f
minhnhut23 <minhnhut23.work@gmail.com>
2024-06-11 23:14:16 +0700

    seed tag data

---
+++ solutions/PostSolution/src/PostService.Infrastructure/Persistence/Mockup/MockupData.cs
@@ -0,0 +1,45 @@
+﻿using PostService.Domain.Entities;
+using PostService.Infrastructure.Persistence.Mockup.Data;
+
+namespace PostService.Infrastructure.Persistence.Mockup;
+
+internal class MockupData
+{
+    private readonly ApplicationDbContext _context;
+    private readonly IUnitOfWork _unitOfWork;
+    public MockupData(ApplicationDbContext context, IUnitOfWork unitOfWork)
+    {
+        _context = context;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task SeedTagData()
+    {
+        await Console.Out.WriteLineAsync("=================Begin seeding tag data=================");
+        foreach (var tag in TagData.Data)
+        {
+            var id = Guid.Parse(tag.Id);
+            var value = tag.Value;
+            var code = tag.Code;
+
+            var isTagExisteed = _context.Tags
+                                        .Where(t => t.Id == id && t.Value == value)
+                                        .SingleOrDefault() != null;
+
+            if (isTagExisteed)
+            {
+                await Console.Out.WriteLineAsync($"Tag {value} is already exited");
+                continue;
+            }
+            Tag tag1 = new Tag{
+                Id = id,
+                Value = value,
+                Code = code
+            };            
+            _context.Tags.Add(tag1);
+            await Console.Out.WriteLineAsync($"Added Tag {value}");
+        }
+        await _unitOfWork.SaveChangeAsync();
+        await Console.Out.WriteLineAsync("=================DONE seeding add data=================");
+    }
+}
