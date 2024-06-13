03f81acfbf2de172160bdcc8296bdb34eadbcc39
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-06-13 13:13:47 +0700

    update context component

--- solutions/ChatHubSolution/src/ChatHub/Program.cs
+++ solutions/ChatHubSolution/src/ChatHub/Program.cs
@@ -1,54 +1,12 @@
-using ChatHub.Hubs;
-using Contract.Event.ConversationEvent;
-using MassTransit;
-using Newtonsoft.Json;
+using ChatHub;
 
-var builder = WebApplication.CreateBuilder(args);
-var services = builder.Services;
-
-services.AddSignalR();
-services.AddHttpContextAccessor();
-
-services.AddSignalR()
-    .AddNewtonsoftJsonProtocol(options =>
-    {
-        options.PayloadSerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-    });
-
-services.AddMassTransit(busConfig =>
-{
-    busConfig.SetKebabCaseEndpointNameFormatter();
-
-    busConfig.UsingRabbitMq((context, config) =>
-    {
-        config.Host(new Uri("amqp://rabbitmq/"), h =>
-        {
-            h.Username("admin");
-            h.Password("pass");
-        });
-
-        config.ConfigureEndpoints(context);
-    });
-
-    busConfig.AddRequestClient<GetConversationByUserIdEvent>(new Uri("queue:get-conversation-by-user-id-event-queue"));
-});
-
-
-services.AddCors(option =>
-{
-    option.AddPolicy(name: "AllowSPAClientOrign", builder =>
-    {
-        builder.WithOrigins("http://localhost:3000", "http://localhost:3001")
-                .AllowAnyHeader()
-                .AllowAnyMethod()
-                .AllowCredentials();
-    });
-});
-
-
+var builder = WebApplication.CreateBuilder(args)
+                .AddChatHubServices();
 var app = builder.Build();
-
-// Set endpoint for a chat hub
-app.MapHub<ChatHubServer>("/chat-hub");
-app.UseCors("AllowSPAClientOrign");
+app.UseChatHubService();
+app.Map("/hello", () =>
+{
+    return "hello";
+});
 app.Run();
+public partial class Program { }
