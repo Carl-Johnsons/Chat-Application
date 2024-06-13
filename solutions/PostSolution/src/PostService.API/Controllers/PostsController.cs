595aa43ca3b73e5a686ede0d4fd5ecf07d670301
minhnhut23 <minhnhut23.work@gmail.com>
2024-06-13 13:45:08 +0700

    update uninteraction post

--- solutions/PostSolution/src/PostService.API/Controllers/PostsController.cs
+++ solutions/PostSolution/src/PostService.API/Controllers/PostsController.cs
@@ -178,4 +178,23 @@ public class PostsController : BaseApiController
 
         return Ok(post.Value);
     }
+
+    [HttpDelete("uninteraction")]
+    public async Task<IActionResult> UninteractionPost([FromBody] UninteractionPostDTO uninteractionPostDTO)
+    {
+        var claims = _httpContextAccessor.HttpContext?.User.Claims;
+        var subjectId = claims?.FirstOrDefault(claim => claim.Type == JwtRegisteredClaimNames.Sub)?.Value;
+
+        var result = await _sender.Send(new UninteractionPostCommand
+        {
+
+            InteractionId = uninteractionPostDTO.InteractionId,
+            PostId = uninteractionPostDTO.PostId,
+            UserId = Guid.Parse(subjectId!)
+        });
+
+
+
+        return Ok();
+    }
 }
---
+++ solutions/PostSolution/src/PostService.Application/Interactions/Commands/UninteractionPostCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using PostService.Domain.Common;
+
+namespace PostService.Application.Interactions.Commands;
+
+public class UninteractionPostCommand : IRequest<Result>
+{
+    public Guid PostId { get; init; }
+    public Guid InteractionId { get; init; }
+    public Guid UserId { get; init; }
+}
+
+public class UninteractionPostCommandHandler : IRequestHandler<UninteractionPostCommand, Result>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UninteractionPostCommandHandler(IApplicationDbContext context, IUnitOfWork unitOfWork)
+    {
+        _context = context;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Result> Handle(UninteractionPostCommand request, CancellationToken cancellationToken)
+    {
+        var result = _context.PostInteracts
+                        .Where(p => p.InteractionId ==  request.InteractionId && p.UserId == request.UserId && p.PostId == request.PostId)
+                        .SingleOrDefault();
+
+        if (result != null)
+        {
+            _context.PostInteracts.Remove(result);
+        }
+        await _unitOfWork.SaveChangeAsync();
+
+        return Result.Success();
+    }
+}
